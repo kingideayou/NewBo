1dbb204f0f96d78b83a28d871a5411c170a58f00
NeXT <kingideayou@qq.com>
2015-04-11 19:49:18 +0800

    StatusBar 和 NavigationBar 透明显示

--- app/src/main/java/com/next/newbo/ui/activity/BaseActivity.java
+++ app/src/main/java/com/next/newbo/ui/activity/BaseActivity.java
@@ -1,21 +1,30 @@
 package com.next.newbo.ui.activity;
 
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 
 import com.next.newbo.R;
+import com.next.newbo.utils.AppLogger;
 import com.next.newbo.utils.Utils;
 import com.next.newbo.support.ShakeDetector;
 import com.next.newbo.ui.utils.DrawerLayoutInstaller;
 import com.next.newbo.ui.view.GlobalMenuView;
+import com.readystatesoftware.systembartint.SystemBarTintManager;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -48,7 +57,51 @@ public class BaseActivity extends ActionBarActivity implements GlobalMenuView.On
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            setTranslucentStatus(true);
+        }
+
+        SystemBarTintManager tintManager = new SystemBarTintManager(this);
+        tintManager.setStatusBarTintEnabled(true);
+        tintManager.setStatusBarTintResource(R.color.style_color_primary_dark);
+
+        tintManager.setNavigationBarTintEnabled(true);
+        tintManager.setNavigationBarTintColor(Color.parseColor("#00000000"));
+
+        /*
+        int actionBarHeight = getActionBarHeight(this);
+        AppLogger.i("mActionBar Height : " + actionBarHeight);
+        tintManager.getConfig().setActionBarHeight(actionBarHeight);
+        */
+        AppLogger.i("ActionBar Height : "+tintManager.getConfig().getActionBarHeight());
+        AppLogger.i("StatusBar Height : " + tintManager.getConfig().getStatusBarHeight());
+
         super.onCreate(savedInstanceState);
+    }
+
+    private int getActionBarHeight(Context context) {
+        int result = 0;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            TypedValue tv = new TypedValue();
+            context.getTheme().resolveAttribute(R.attr.actionBarSize, tv, true);
+//                context.getTheme().resolveAttribute(android.support.v7.appcompat.R.attr.actionBarSize, tv, true);
+            result = TypedValue.complexToDimensionPixelSize(tv.data, context.getResources().getDisplayMetrics());
+        }
+        return result;
+    }
+
+    @TargetApi(19)
+    private void setTranslucentStatus(boolean on) {
+        Window win = getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        win.setAttributes(winParams);
     }
 
     @Override
